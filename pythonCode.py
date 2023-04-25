291ff85e894457d8eff9334e77041d9c673b036e
aksingh002 <aksinghcool007@gmail.com>
2023-04-25 18:05:06 +0530

    this is the final commit

--- pythonCode.py
+++ pythonCode.py
@@ -1,30 +1,67 @@
 import csv
+import re
+import time
+import psutil
 
-# Open the input text file
-with open('shakespeare.txt', 'r') as f:
-    shakespeare = f.read()
+# Define file paths
+input_file_path = "t8.shakespeare.txt"
+find_words_list_file_path = "find_words.txt"
+dictionary_file_path = "french_dictionary.csv"
+output_file_path = "t8.shakespeare.translated.txt"
+frequency_file_path = "frequency.csv"
 
-# Open the words list text file
-with open('find_words.txt', 'r') as f:
-    find_words = f.read().split()
+# Load find words list
+with open(find_words_list_file_path, "r") as f:
+    find_words = set(line.strip() for line in f)
 
-# Load the dictionary CSV file into a dictionary
-french_dictionary = {}
-with open('french_dictionary.csv', 'r') as f:
+# Load dictionary
+with open(dictionary_file_path, "r") as f:
     reader = csv.reader(f)
-    for row in reader:
-        french_dictionary[row[0]] = row[1]
+    dictionary = {row[0]: row[1] for row in reader}
 
-# Find all the words in the input text file that are in the words list and have a replacement word in the dictionary
-replacements = {}
-for word in shakespeare:
-    if word in find_words and word in french_dictionary:
-        replacements[word] = french_dictionary[word]
+# Define regular expression for finding words
+word_pattern = re.compile(r"\b\w+\b")
 
-# Replace the words in the input text file with their corresponding replacement words from the dictionary
-for word, replacement in replacements.items():
-    shakespeare = shakespeare.replace(word, replacement)
+# Define counters
+replacements = 0
+unique_replacements = set()
+word_counts = {}
 
-# Write the modified input text to a new file
-with open('output.txt', 'w') as f:
-    f.write(shakespeare)
+# Process input file
+with open(input_file_path, "r") as f_in, open(output_file_path, "w") as f_out:
+    for line in f_in:
+        words = word_pattern.findall(line)
+        for word in words:
+            if word in find_words and word.lower() in dictionary:
+                replacement = dictionary[word.lower()]
+                if word.istitle():
+                    replacement = replacement.capitalize()
+                elif word.isupper():
+                    replacement = replacement.upper()
+                line = line.replace(word, replacement)
+                replacements += 1
+                unique_replacements.add(word)
+                if word in word_counts:
+                    word_counts[word] += 1
+                else:
+                    word_counts[word] = 1
+
+        f_out.write(line)
+
+# Write frequency file
+with open(frequency_file_path, "w", newline='') as f_freq:
+    writer = csv.writer(f_freq)
+    writer.writerow(["English Word", "French Word", "Frequency"])
+    for word, count in word_counts.items():
+        if word in dictionary:
+            writer.writerow([word, dictionary[word], count])
+
+# End timing and memory usage
+end_time = time.time()
+end_memory = psutil.Process().memory_info().rss
+
+# Print results
+print(f"Unique list of words that was replaced with French words from the dictionary: {unique_replacements}")
+print(f"Number of times a word was replace: {replacements}")
+print(f"Time taken to process: {end_time -end_time:.2f} seconds")
+print(f"Memory taken to process: {(end_memory -end_memory) / (1024*1024):.2f} MB")
